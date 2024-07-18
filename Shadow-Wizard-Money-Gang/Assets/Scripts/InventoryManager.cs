5ec54f450b6dde4c76313781b37945fc63941e9b
Memesitos <diegocacho76@gmail.com>
2024-07-17 19:24:31 -0500

    Inventory - 1

---
+++ Shadow-Wizard-Money-Gang/Assets/Scripts/InventoryManager.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InventoryManager
+{
+    private List<Item> itemList;
+
+    public InventoryManager()
+    {
+        itemList = new List<Item>();
+
+        //Testing
+        Debug.Log("InventoryManager");
+    }
+
+
+
+
+}
