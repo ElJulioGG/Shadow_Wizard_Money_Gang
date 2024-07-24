0383a9f98b9c6e8b8fad633a61e598e39b866519
Memesitos <diegocacho76@gmail.com>
2024-07-24 14:41:04 -0500

    Inventory UI - Drag/Drop items
    Added the option to drag/drop the items around the screen, still need to add proper grids so the player can't place them outside the inventory UI

--- Shadow-Wizard-Money-Gang/Assets/Scripts/DragDrop/DragDrop.cs
+++ Shadow-Wizard-Money-Gang/Assets/Scripts/DragDrop/DragDrop.cs
@@ -3,11 +3,51 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
-public class DragDrop : MonoBehaviour, IPointerDownHandler
+public class DragDrop : MonoBehaviour, IPointerDownHandler, IBeginDragHandler, IEndDragHandler, IDragHandler
 {
+    [SerializeField] private Canvas canvas;
+
+    private RectTransform rectTransform;
+    private CanvasGroup canvasGroup;
+
+    private void Awake()
+    {
+        rectTransform = GetComponent<RectTransform>();
+        canvasGroup = GetComponent<CanvasGroup>();
+    }
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        Debug.Log("OnBeginDrag");
+        canvasGroup.alpha = .6f;
+        canvasGroup.blocksRaycasts = false;
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        Debug.Log("OnDrag");
+        rectTransform.anchoredPosition += eventData.delta / canvas.scaleFactor;
+        //rectTransform.anchoredPosition += eventData.delta * Time.deltaTime;
+        //this.transform.position = eventData.position;
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        Debug.Log("OnEndDrag");
+        canvasGroup.alpha = 1f;
+        canvasGroup.blocksRaycasts = true;
+    }
+
     public void OnPointerDown(PointerEventData eventData)
     {
         Debug.Log("OnPointerDown");
-        //throw new System.NotImplementedException();
     }
 }
+
+//Tip: by default unity has a threshold on drag, meaning the object starts
+//following after the mouse has moved an amount in some direction.
+//To avoid this and have the object move exactly under where you click,
+//you need to implement the IInitializePotentialDragHandler interface and
+//inside OnInitializePotentialDrag set eventData.useDragThreshold = false
+
+//, IInitializePotentialDragHandler
